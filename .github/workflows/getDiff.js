57e457d4cdfbffb4adfbfbe5771983e28d5eaf74
Noelia Ruiz Martínez <nrm1977@gmail.com>
2025-08-14 19:22:58 +0200

    Es diff2

--- .github/workflows/getDiff.js
+++ .github/workflows/getDiff.js
@@ -2,6 +2,7 @@ module.exports = getDiff
 const fs = require('fs')
 const differenceWith = require('lodash/differenceWith')
 const isEqual = require('lodash/isEqual')
+const find = require('lodash/find')
 
 
 function getDiff() {
@@ -9,10 +10,13 @@ function getDiff() {
 	let data1 = JSON.parse(fileContents)
 	fileContents = fs.readFileSync('new.json')
 	let data2 = JSON.parse(fileContents)
+	fileContents = fs.readFileSync('es.json')
+	let dataEs = JSON.parse(fileContents)
 	let diff = differenceWith(data2, data1, isEqual)
-	diff.forEach((item) => console.log(item.sha256))
 	let mailBody = ``
-	diff.forEach((item) => mailBody += `${item.addonId} ${item.addonVersionName}\n$${item.description}\n`)
-	console.log(mailBody)
+	diff.forEach((item) => mailBody += `${item.addonId} ${item.addonVersionName}\n${item.description}\n\n`)
+	esDiff = diff.map((item) => find(dataEs, (o) => o.sha256 === item.sha256), item.sha256)
+	console.log(esDiff)
+
 	return diff
 }
