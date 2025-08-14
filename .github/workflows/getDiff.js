96604cb2832e2ba794fad48cef231c36cc776ba6
Noelia Ruiz Martínez <nrm1977@gmail.com>
2025-08-14 09:57:57 +0200

    Test with lodash

---
+++ .github/workflows/getDiff.js
@@ -0,0 +1,14 @@
+module.exports = getDiff
+const fs = require('fs')
+const fs = require('fs')
+const lodash = require('lodash')
+
+
+function getDiff() {
+	let fileContents = fs.readFileSync(first.json)
+	let data1 = JSON.parse(fileContents)
+	let fileContents = fs.readFileSync(last.json)
+	let data2 = JSON.parse(fileContents)
+	const rejected = lodash._reject(data2, function(o) { return data1._find(o)})
+	console.log(rejected.length)
+}
