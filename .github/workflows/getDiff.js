654eaaacc2eb6a05512657c641d8770cede80c3d
Noelia Ruiz Martínez <nrm1977@gmail.com>
2025-08-14 12:15:55 +0200

    Test

--- .github/workflows/getDiff.js
+++ .github/workflows/getDiff.js
@@ -4,11 +4,10 @@ const difference = require('lodash/difference')
 
 
 function getDiff() {
-	let fileContents = fs.readFileSync('first.json')
+	let fileContents = fs.readFileSync('old.json')
 	let data1 = JSON.parse(fileContents)
-	fileContents = fs.readFileSync('latest.json')
+	fileContents = fs.readFileSync('new.json')
 	let data2 = JSON.parse(fileContents)
 	let diff = difference(data2, data1)
-	console.log(diff.length)
 	return diff
 }
