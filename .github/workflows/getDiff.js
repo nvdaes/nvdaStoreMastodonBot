f4b651545bb47afefee2324bcdc23d09284407bb
Noelia Ruiz Martínez <nrm1977@gmail.com>
2025-08-14 10:38:48 +0200

    Fix

--- .github/workflows/getDiff.js
+++ .github/workflows/getDiff.js
@@ -6,7 +6,7 @@ const lodash = require('lodash')
 function getDiff() {
 	let fileContents = fs.readFileSync(first.json)
 	let data1 = JSON.parse(fileContents)
-	let fileContents = fs.readFileSync(last.json)
+	fileContents = fs.readFileSync(last.json)
 	let data2 = JSON.parse(fileContents)
 	const rejected = lodash._reject(data2, function(o) { return data1._find(o)})
 	console.log(rejected.length)
